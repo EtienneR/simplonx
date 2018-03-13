f594d1e9c46d60d51b6972ce71c85d0ced86236b
EtienneR <etienne.rouzeaud@free.fr>
2018-03-13 12:14:15 +0100

    Ajout VueJS

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
-
+const path = require('path')
 const app = express()
 
+app.use('/', express.static(path.join(__dirname + '/front/dist')))
+
 app.get('*', (req, res) => {
-    res.send('Coucou la démo')
+    //res.send('Coucou la démo')
+    res.sendFile(path.join(__dirname + '/front/dist/index.html'))
 })
 
 const port = process.env.PORT || 3000
